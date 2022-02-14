08e7383653503c329862b701c3615a3dc160ee2c
Dani Vainstein <solmarkn@gmail.com>
2022-02-14 01:17:06 -0500

    Update argtypes.py

--- sel4/contrib/argparse/argtypes.py
+++ sel4/contrib/argparse/argtypes.py
@@ -28,14 +28,19 @@ class PositiveIntArgType:
             raise ValueError(str(e))
 
     def __repr__(self) -> str:
-        return '[PositiveInt type]'
+        return "[PositiveInt type]"
 
 
 class ConstrainedIntArgType:
     def __init__(
-            self, *,
-            strict: bool = False, gt: int = None, ge: int = None, lt: int = None, le: int = None,
-            multiple_of: int = None
+        self,
+        *,
+        strict: bool = False,
+        gt: int = None,
+        ge: int = None,
+        lt: int = None,
+        le: int = None,
+        multiple_of: int = None
     ):
         class ConstrainedIntValidator(pydantic.BaseModel):
             constrained_int: pydantic.ConstrainedInt = pydantic.conint(
@@ -53,25 +58,33 @@ class ConstrainedIntArgType:
             raise ValueError(str(e))
 
     def __repr__(self) -> str:
-        return '[ConstrainedIntArgType type]'
+        return "[ConstrainedIntArgType type]"
 
 
 class ConstrainedNumberMeta:
-    def __init__(self, *, gt: OptionalInt, ge: OptionalInt, lt: OptionalInt, le: OptionalInt):
+    def __init__(
+        self, *, gt: OptionalInt, ge: OptionalInt, lt: OptionalInt, le: OptionalInt
+    ):
         if gt is not None and ge is not None:
-            raise argparse.ArgumentTypeError('bounds gt and ge cannot be specified at the same time')
+            raise argparse.ArgumentTypeError(
+                "bounds gt and ge cannot be specified at the same time"
+            )
         if lt is not None and le is not None:
-            raise argparse.ArgumentTypeError('bounds lt and le cannot be specified at the same time')
+            raise argparse.ArgumentTypeError(
+                "bounds lt and le cannot be specified at the same time"
+            )
 
 
 class ConstrainedIntType(ConstrainedNumberMeta):
-
     def __init__(
-            self, *,
-            strict: bool = False,
-            gt: OptionalInt = None, ge: OptionalInt = None,
-            lt: OptionalInt = None, le: OptionalInt = None,
-            multiple_of: OptionalInt = None
+        self,
+        *,
+        strict: bool = False,
+        gt: OptionalInt = None,
+        ge: OptionalInt = None,
+        lt: OptionalInt = None,
+        le: OptionalInt = None,
+        multiple_of: OptionalInt = None
     ):
         super().__init__(gt=gt, ge=ge, lt=lt, le=le)
         self.strict = strict
@@ -82,9 +95,11 @@ class ConstrainedIntType(ConstrainedNumberMeta):
         self.multiple_of = multiple_of
 
     def __call__(self, v: Union[str, int]) -> Union[int, None]:
-        validator = validators.strict_int_validator if self.strict else validators.int_validator
+        validator = (
+            validators.strict_int_validator if self.strict else validators.int_validator
+        )
         value = validator(v)
         return value
 
     def __repr__(self) -> str:
-        return '[integer type]'
+        return "[integer type]"
