ae8470fdf96d1df23caa66d6cf6ec1fd629e4a36
Dani Vainstein <solmarkn@gmail.com>
2022-02-14 01:17:04 -0500

    Update __init__.py

--- sel4/__init__.py
+++ sel4/__init__.py
@@ -4,14 +4,16 @@ import pathlib
 from .utils.envutils import env
 
 if env("PYTHONPATH", str, None):
+
     def _add_to_python_path():
         root_path = str(pathlib.Path(__file__).parent.parent)
-        if 'PROJECT_ROOT' not in os.environ:
-            os.environ.setdefault('PROJECT_ROOT', root_path)
+        if "PROJECT_ROOT" not in os.environ:
+            os.environ.setdefault("PROJECT_ROOT", root_path)
         sel4_path = str(pathlib.Path(__file__).parent)
-        if sel4_path not in os.environ['PYTHONPATH']:
-            os.environ["PYTHONPATH"] = f'{root_path}:{sel4_path}:' + os.environ['PYTHONPATH']
-
+        if sel4_path not in os.environ["PYTHONPATH"]:
+            os.environ["PYTHONPATH"] = (
+                f"{root_path}:{sel4_path}:" + os.environ["PYTHONPATH"]
+            )
 
     _add_to_python_path()
 else:
@@ -20,8 +22,8 @@ else:
     def _create_python_path():
         root_path = str(pathlib.Path(__file__).parent.parent)
         sel4_path = str(pathlib.Path(__file__).parent)
-        os.environ.setdefault('PROJECT_ROOT', root_path)
-        os.environ.setdefault('PYTHONPATH', f'{root_path}:{sel4_path}:')
+        os.environ.setdefault("PROJECT_ROOT", root_path)
+        os.environ.setdefault("PYTHONPATH", f"{root_path}:{sel4_path}:")
         sys.path.append(str(root_path))
         sys.path.append(str(sel4_path))
 
@@ -30,8 +32,10 @@ else:
 from rich import get_console, reconfigure
 from rich.console import Console
 from rich.traceback import install
+
 from sel4.contrib.pydantic.validators import register_validators
-from .utils.log import setup_stderr_handler, setup_bootstrap_logger
+
+from .utils.log import setup_bootstrap_logger, setup_stderr_handler
 
 # print_rich_256_color_names()
 
