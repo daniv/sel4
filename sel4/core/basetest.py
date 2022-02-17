aa00bf328c506de73d6b626a0620cc0b6acd32b2
Dani Vainstein <solmarkn@gmail.com>
2022-02-16 23:16:35 -0500

    Update basetest.py

--- sel4/core/basetest.py
+++ sel4/core/basetest.py
@@ -1,15 +1,21 @@
+import time
+
 from _pytest import unittest
+from loguru import logger
 from pydantic import Field, validate_arguments, PositiveInt
+from rich.highlighter import ReprHighlighter
 
 from sel4.utils.typeutils import OptionalFloat, OptionalInt
 
 from .runtime import runtime_store
+from ..conf import settings
 
 
 class BasePytestUnitTestCase(unittest.UnitTestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self._highlighter = ReprHighlighter()
         self._called_setup = False
         self._called_teardown = False
         self._is_timeout_changed = False
@@ -57,5 +63,25 @@ class BasePytestUnitTestCase(unittest.UnitTestCase):
         if not timeout:
             return default_tm
         if self.config.getoption("timeout_multiplier", None) and timeout == default_tm:
+            logger.debug("Recalculating new timeout")
             return self.__get_new_timeout(timeout)
         return timeout
+
+    @staticmethod
+    def get_beautiful_soup(source: str):
+        """
+        BeautifulSoup is a toolkit for dissecting an HTML document
+        and extracting what you need. It's great for screen-scraping!
+        See: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+        """
+        from bs4 import BeautifulSoup
+        logger.debug("Create instance of BeautifulSoup base on page source")
+        soup = BeautifulSoup(source, "html.parser")
+        return soup
+
+    def _slow_mode_pause_if_active(self):
+        if self.config.getoption("slow_mode", False):
+            wait_time = settings.DEFAULT_DEMO_MODE_TIMEOUT
+            if self.config.getoption("demo_sleep", False):
+                wait_time = float(self.config.getoption("demo_sleep"))
+            time.sleep(wait_time)
